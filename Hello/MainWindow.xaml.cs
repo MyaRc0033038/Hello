ea50fb05bee135a346dcf16661e3f9c0833d009e
Mya Perry <rc0033038@student.riversidecollege.ac.uk>
2018-11-19 15:20:39 +0000

    Desplay "Hello"

--- Hello/MainWindow.xaml.cs
+++ Hello/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Hello
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
